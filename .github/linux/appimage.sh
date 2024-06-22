2ab6ec8fd24488d689643c6d2467c0a1ec5fe3ee
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2024-06-22 14:52:28 -0700

    Update appimage.sh

--- .github/linux/appimage.sh
+++ .github/linux/appimage.sh
@@ -1,37 +1,16 @@
-ARCH=$(uname -m)
-LINUX_DEPLOY_ARCH=$(uname -m)
-
-if [ "$ARCH" == "x86_64" ]; then
-  ARCH="x86_64"
-  LINUX_DEPLOY_ARCH="x86_64"
-elif [ "$ARCH" == "aarch64" ]; then
-  ARCH="arm_aarch64"
-  LINUX_DEPLOY_ARCH="aarch64"
-else
-  echo "Unsupported architecture: $ARCH"
-  exit 1
-fi
-
-curl -sSfLO "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-$LINUX_DEPLOY_ARCH.AppImage"
+curl -sSfLO "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-static-x86_64.AppImage"
 curl -sSfLO "https://github.com/linuxdeploy/linuxdeploy-plugin-gtk/raw/master/linuxdeploy-plugin-gtk.sh"
 chmod a+x linuxdeploy*
  
 mkdir -p AppDir/usr/bin
-cp Zelda64Recompiled AppDir/usr/bin/
+cp DrMario64 AppDir/usr/bin/
 cp -r assets/ AppDir/usr/bin/
-cp icons/512.png AppDir/Zelda64Recompiled.png
-cp .github/linux/Zelda64Recompiled.desktop AppDir/
+cp .github/linux/{DrMario64.desktop,DrMario64.png} AppDir/
 
-"./linuxdeploy-$LINUX_DEPLOY_ARCH.AppImage" --appimage-extract
+./linuxdeploy-static-x86_64.AppImage --appimage-extract
 mv squashfs-root/ deploy
-./deploy/AppRun --appdir=AppDir/ -d AppDir/Zelda64Recompiled.desktop -i AppDir/Zelda64Recompiled.png -e AppDir/usr/bin/Zelda64Recompiled --plugin gtk
+ARCH=x86_64 ./deploy/AppRun --appdir=AppDir/ -d AppDir/DrMario64.desktop -i AppDir/DrMario64.png -e AppDir/usr/bin/DrMario64 --plugin gtk
 sed -i 's/exec/#exec/g' AppDir/AppRun
-echo 'if [ -f "portable.txt" ]; then' >> AppDir/AppRun
-echo '    APP_FOLDER_PATH=$PWD' >> AppDir/AppRun
-echo '    cd "$this_dir"/usr/bin/' >> AppDir/AppRun
-echo '    APP_FOLDER_PATH=$APP_FOLDER_PATH ./Zelda64Recompiled' >> AppDir/AppRun
-echo 'else' >> AppDir/AppRun
-echo '    cd "$this_dir"/usr/bin/' >> AppDir/AppRun
-echo '    ./Zelda64Recompiled' >> AppDir/AppRun
-echo 'fi' >> AppDir/AppRun
-./deploy/usr/bin/linuxdeploy-plugin-appimage --appdir=AppDir
+echo 'cd "$this_dir"/usr/bin/' >> AppDir/AppRun
+echo './DrMario64' >> AppDir/AppRun
+ARCH=x86_64 ./deploy/usr/bin/linuxdeploy-plugin-appimage --appdir=AppDir
